afb20ecda82db1d6c8100810d1b2a8d768af5128
Thiti Prasopsub <auethiti@gmail.com>
2025-04-29 21:03:17 +0700

    044 - Add BE

---
+++ BE/itb-mshop-be/src/main/java/org/example/itbmshopbe/ItbMshopBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.itbmshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbMshopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbMshopBeApplication.class, args);
+    }
+
+}
---
+++ BE/itb-mshop-be/src/test/java/org/example/itbmshopbe/ItbMshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.itbmshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbMshopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
