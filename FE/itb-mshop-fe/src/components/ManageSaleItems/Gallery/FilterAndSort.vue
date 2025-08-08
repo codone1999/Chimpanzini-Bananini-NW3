69b6ef8f37f95f9973940a779bf0ba6587433c65
Natkamon Chotipiboonsub <natkamon.chot@kmutt.ac.th>
2025-08-08 12:13:24 +0700

    028 - [Update] Decorate filtering UI

--- FE/itb-mshop-fe/src/components/ManageSaleItems/Gallery/FilterAndSort.vue
+++ FE/itb-mshop-fe/src/components/ManageSaleItems/Gallery/FilterAndSort.vue
@@ -61,15 +61,15 @@ function handlePageSizeChange(event) {
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row justify-between gap-5 items-start w-full mb-8">
+  <div class="flex flex-col md:flex-row justify-between gap-4 items-start w-full mb-8 bg-[#1e1e1e] p-4 rounded-lg shadow-md">
 
     <!-- Filter Section (Expandable Left Side) -->
-    <div class="flex items-center gap-2 flex-grow ">
+    <div class="flex items-center gap-3 flex-grow w-full">
 
       <!-- Each Pill will expand equally -->
-      <div class="flex-1 border rounded-full px-6 py-1.5 bg-white shadow-sm text-left">
-        <div class="text-sm font-semibold text-gray-800">Brand</div>
-        <div class="text-xs text-gray-400 truncate">
+      <div class="flex-1 border rounded-full px-6 py-1.5 border-gray-700 bg-gray-100 shadow-sm text-left hover:border-purple-500 transition-all duration-200">
+        <div class="text-sm font-medium text-gray-800">Brand</div>
+        <div class="text-xs text-gray-400 mt-1">
           <span v-if="props.filterBrands.length === 0">Filter by brand(s)</span>
           <span v-else>
             {{ props.filterBrands.join(', ') }}
@@ -77,9 +77,9 @@ function handlePageSizeChange(event) {
         </div>
       </div>
 
-      <div class="flex-1 border rounded-full px-6 py-1.5 bg-white shadow-sm text-left">
-        <div class="text-sm font-semibold text-gray-800">Price</div>
-        <div class="text-xs text-gray-400">Price Range
+      <div class="flex-1 border rounded-full px-6 py-1.5 border-gray-700 bg-gray-100 shadow-sm text-left hover:border-purple-500 transition-all duration-200">
+        <div class="text-sm font-medium text-gray-800">Price</div>
+        <div class="text-xs text-gray-400 mt-1">Price Range
           <!-- NOTE - add filterPrice in ListGallery -->
           <!-- <span v-if="props.filterPrice.length === 0">Price Range</span>
           <span v-else>
@@ -88,34 +88,39 @@ function handlePageSizeChange(event) {
         </div>
       </div>
 
-      <div class="flex-1 border rounded-full px-6 py-1.5 bg-white shadow-sm text-left">
-        <div class="text-sm font-semibold text-gray-800">Storage Size</div>
-        <div class="text-xs text-gray-400">Storage Range
-          <!-- NOTE - add filterStorageSize in ListGallery -->
-          <!-- <span v-if="props.filterStorageSize.length === 0">Storage Range</span>
-          <span v-else>
-            {{ props.filterStorageSize.join(', ') }}
-          </span> -->
+      <div class="flex-1 border rounded-full px-6 py-1.5 border-gray-700 bg-gray-100 shadow-sm text-left hover:border-purple-500 transition-all duration-200">
+        <div class="flex items-center justify-between">
+          <div>
+            <div class="text-sm font-medium text-gray-800">Storage Size</div>
+            <div class="text-xs text-gray-400 mt-1">Storage Range
+              <!-- NOTE - add filterStorageSize in ListGallery -->
+              <!-- <span v-if="props.filterStorageSize.length === 0">Storage Range</span>
+              <span v-else>
+                {{ props.filterStorageSize.join(', ') }}
+              </span> -->
+            </div>
+          </div>
+          <button
+            @click="props.toggleBrandList"
+            class="-mr-4 pt-2 px-2 pb-1 bg-gray-200 border border-gray-600 rounded-full hover:bg-gray-300 transition"
+          >
+            <span class="material-icons text-gray-700">filter_alt</span>
+          </button>
         </div>
       </div>
 
       <!-- Filter Icon -->
-      <button
-        @click="props.toggleBrandList"
-        class="p-3 flex items-center justify-center bg-gray-200 border border-gray-600 rounded-full hover:bg-gray-300 transition"
-      >
-        <span class="material-icons text-gray-700">filter_alt</span>
-      </button>
+      
     </div>
 
     <!-- Sort + Page Size (Right Side) -->
     <div class="flex md:flex-nowrap gap-2 items-center w-full md:w-auto">
       <div class="flex items-center gap-2 text-base">
-        <label for="pageSize" class="font-medium text-gray-700 whitespace-nowrap">Show:</label>
+        <label for="pageSize" class="font-medium text-purple-400 text-shadow-white whitespace-nowrap">Show:</label>
         <select
           :value="props.pageSize"
           @change="handlePageSizeChange"
-          class="itbms-page-size border border-gray-300 rounded pl-3 pr-2 py-3 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          class="itbms-page-size border border-gray-300 bg-gray-100 rounded pl-3 pr-2 py-3 focus:outline-none focus:ring-2 focus:ring-purple-500"
         >
           <option :value="5">5</option>
           <option :value="10">10</option>
@@ -127,7 +132,7 @@ function handlePageSizeChange(event) {
       <button
         class="itbms-brand-none px-3 py-2 text-2xl rounded transition"
         :class="props.sortMode === 'none'
-          ? 'bg-white text-black border border-gray-300'
+          ? 'bg-gray-100 text-black border border-gray-300'
           : 'bg-[#7e5bef] text-white hover:bg-[#6847d5]'"
         @click="props.onChangeSort('none')"
         title="No Sort"
