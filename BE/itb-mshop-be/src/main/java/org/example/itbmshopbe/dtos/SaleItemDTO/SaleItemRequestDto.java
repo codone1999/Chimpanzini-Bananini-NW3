1c66a7583127f816c731378cf1f2af61a1e7948d
Thiti Prasopsub <104205131+codone1999@users.noreply.github.com>
2025-09-23 14:48:59 +0700

    044-update-update add saleitem by seller to be vaild

--- BE/itb-mshop-be/src/main/java/org/example/itbmshopbe/dtos/SaleItemDTO/SaleItemRequestDto.java
+++ BE/itb-mshop-be/src/main/java/org/example/itbmshopbe/dtos/SaleItemDTO/SaleItemRequestDto.java
@@ -1,5 +1,6 @@
 package org.example.itbmshopbe.dtos.SaleItemDTO;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.example.itbmshopbe.dtos.BrandDTO.BrandDto;
@@ -8,19 +9,23 @@ import org.example.itbmshopbe.dtos.BrandDTO.BrandDto;
 public class SaleItemRequestDto {
     private Integer id;
 
+    @Valid
     @NotBlank(message = "Model is required")
     @Size(max = 60, message = "Model must be less than 60 characters")
     private String model;
 
+    @Valid
     @NotNull(message = "Brand information is required")
     private BrandDto brand;
 
     @Size(max = 60, message = "Brand name must be less than 60 characters")
     private String brandName;
 
+    @Valid
     @NotBlank(message = "Description is required")
     private String description;
 
+    @Valid
     @NotNull(message = "Price is required")
     @Min(value = 0, message = "Price must be a positive value")
     private Integer price;
@@ -29,6 +34,7 @@ public class SaleItemRequestDto {
 
     private Double screenSizeInch;
 
+    @Valid
     @NotNull(message = "Quantity is required")
     @Min(value = 0, message = "Quantity must be a positive value")
     private Integer quantity;
@@ -37,7 +43,6 @@ public class SaleItemRequestDto {
 
     @Size(max = 255, message = "Color cannot exceed 255 characters")
     private String color;
-
-    //private Integer sellerId;
+    
 }
 
