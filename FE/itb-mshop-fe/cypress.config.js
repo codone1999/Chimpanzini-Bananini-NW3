49aa111cb6b2c13b7e5096ad6c1de68c1569ed0e
BOOMGOGGAG <songkiatchai.wong@kmutt.ac.th>
2025-09-28 17:02:56 +0700

    035-[Update] Change Path Protocol to https
    - Cypress
    - application.properties
    - .env

--- FE/itb-mshop-fe/cypress.config.js
+++ FE/itb-mshop-fe/cypress.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     // baseUrl: 'http://ip24nw3.sit.kmutt.ac.th',
     // baseAPI: 'http://ip24nw3.sit.kmutt.ac.th:8080',
-    baseUrl: 'http://intproj24.sit.kmutt.ac.th/nw3/',
-    baseAPI: 'http://intproj24.sit.kmutt.ac.th/nw3/api/',
+    baseUrl: 'https://intproj24.sit.kmutt.ac.th/nw3/',
+    baseAPI: 'https://intproj24.sit.kmutt.ac.th/nw3/api/',
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
